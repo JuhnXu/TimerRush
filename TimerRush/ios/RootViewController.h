6c67978bf352740f672dbb6be5e512865b52d0c0
JuhnXu <JuhnXu@qq.com>
2013-05-16 00:47:12 +0800

    Initial Commit

---
+++ TimerRush/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  TimerRushAppController.h
+//  TimerRush
+//
+//  Created by JuhnXu on 13-5-16.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
